947fccb060133a229005332c4b2c27a7f812df51
Reza Tavasoli <jfk998@gmail.com>
2019-08-12 12:19:42 +0430

    Update GameState

--- gamestate.py
+++ gamestate.py
@@ -1,6 +1,13 @@
+xAxis, yAxis = 3, 2
+
+
 class GameState:
+
     def __init__(self):
-        pass
+        self._board = [[0] * yAxis for _ in range(xAxis)]
+        self._board[-1][-1] = 1
+        self._activePlayer = 0
+        self._player_locations = [None, None]
 
 
 if __name__ == '__main__':
